2c8cdb6de7c176d9af590ba02002e5a03dc78d4c
arielbeje <arielb624@gmail.com>
2017-06-14 18:07:53 +0300

    Commented out the other person tagging, broke nikey tagging

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ client = discord.Client()
 
 YourID = "" #Get this id by using \@yourname and copying only the numbers
 NikeyID = "" #Get this id by using \@Nikey and copying the whole message, includinga the <, @, >
-OtherPersonID = "" #Same as NikeyID, but for any person you want
-OtherPersonTag = "" #Whatever you want to tag so the bot will change it to the other person
+#OtherPersonID = "" #Same as NikeyID, but for any person you want
+#OtherPersonTag = "" #Whatever you want to tag so the bot will change it to the other person
 token = "" #To get your token, press ctrl/cmd + shift + I, press on the little arrow, and in the Application tab, pick Local Storage, the link below it, and scroll down to see token
 
 @client.event
@@ -22,8 +22,8 @@ async def on_message (message):
 			await client.delete_message(message)
 			await client.send_message(message.channel, msg.replace("@nikey", "<@" + NikeyID + ">"))
 
-		elif "@" + OtherPersonTag in message.content:
-			await client.delete_message(message)
-			await client.send_message(message.channel, msg.replace("@:)", "<@" + OtherPersonID + ">"))
+		#elif "@" + OtherPersonTag in message.content:
+		#	await client.delete_message(message)
+		#	await client.send_message(message.channel, msg.replace("@:)", "<@" + OtherPersonID + ">"))
 
 client.run(token, bot=False)
