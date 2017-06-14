95517e329059ff1bfcc0f2028e95da06675b684f
arielbeje <arielb624@gmail.com>
2017-06-14 16:31:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+def yourID="" #Get this id by using /<@yourname> and copying only the numbers
+def NikeyID="" #Get this id by using /<@Nikey> and copying the whole message, including the <, @, >
+
+@client.event
+async def on_message (message):
+	if message.author.id == YourID:
+		print(message.content)
+		msg=message.content
+
+		if "@Nikey" in message.content:
+			await client.delete_message(message)
+			await client.send_message(message.channel, msg.replace("@Nikey", NikeyID))
+			
+		elif "@nikey" in message.content:	
+			await client.delete_message(message)
+			await client.send_message(message.channel, msg.replace("@nikey", NikeyID))
+
+client.run('token', bot=False) #To get your token, press ctrl/cmd + shift + I, press on the little arrow, and in the Application tab, pick Local Storage, the link below it, and scroll down to see token
---
+++ readme.md
@@ -0,0 +1,16 @@
+# TagNikeyBot
+We all know Nikey. That annoying guy with the Unicode characters name.
+
+I decided to end his reign as a hard to mention person, and make a bot to easily mention him.
+
+# Requirements
+[Python 3](https://www.python.org/downloads/)
+
+[Discord.py](https://github.com/Rapptz/discord.py)
+
+# Guide
+If you're on Windows, run run.bat file.
+
+If you're on Linux, run the linuxrun file.
+
+If you're on mac, idk.
