a9bc83efd662c5138abacb3388749e81f0525ca3
arielbeje <arielb624@gmail.com>
2017-06-14 17:07:07 +0300

    Added an option for another person

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ client = discord.Client()
 
 YourID = "" #Get this id by using \@yourname and copying only the numbers
 NikeyID = "" #Get this id by using \@Nikey and copying the whole message, includinga the <, @, >
+OtherPersonID = "" #Same as NikeyID, but for any person you want
+OtherPersonTag = "" #Whatever you want to tag so the bot will change it to the other person
+token = "" #To get your token, press ctrl/cmd + shift + I, press on the little arrow, and in the Application tab, pick Local Storage, the link below it, and scroll down to see token
 
 @client.event
 async def on_message (message):
@@ -13,10 +16,14 @@ async def on_message (message):
 
 		if "@Nikey" in message.content:
 			await client.delete_message(message)
-			await client.send_message(message.channel, msg.replace("@Nikey", NikeyID))
+			await client.send_message(message.channel, msg.replace("@Nikey", "<@" + NikeyID + ">"))
 			
 		elif "@nikey" in message.content:	
 			await client.delete_message(message)
-			await client.send_message(message.channel, msg.replace("@nikey", NikeyID))
+			await client.send_message(message.channel, msg.replace("@nikey", "<@" + NikeyID + ">"))
 
-client.run('token', bot=False) #To get your token, press ctrl/cmd + shift + I, press on the little arrow, and in the Application tab, pick Local Storage, the link below it, and scroll down to see token
+		elif "@" + OtherPersonTag in message.content:
+			await client.delete_message(message)
+			await client.send_message(message.channel, msg.replace("@:)", "<@" + OtherPersonID + ">"))
+
+client.run(token, bot=False)
