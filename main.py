017391800bcc935ce68c854693a147bb48d811e9
arielbeje <arielb624@gmail.com>
2017-06-15 13:08:45 +0300

    Made the tag case insensitive

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import discord
 import asyncio
+import re
 
 client = discord.Client()
+
+token = "" #To get your token, press ctrl/cmd + shift + I, press on the little arrow, and in the Application tab, pick Local Storage, the link below it, and scroll down to see token
 
 YourID = "" #Get this id by using \@yourname and copying only the numbers
 NikeyID = "" #Get this id by using \@Nikey and copying the whole message, includinga the <, @, >
 #OtherPersonID = "" #Same as NikeyID, but for any person you want
 #OtherPersonTag = "" #Whatever you want to tag so the bot will change it to the other person
-token = "" #To get your token, press ctrl/cmd + shift + I, press on the little arrow, and in the Application tab, pick Local Storage, the link below it, and scroll down to see token
 
 @client.event
 async def on_message (message):
 	if message.author.id == YourID:
 		msg = message.content
+		lmsg = message.content.lower()
 
-		if "@Nikey" in message.content:
+		if "@nikey" in lmsg:
 			await client.delete_message(message)
-			await client.send_message(message.channel, msg.replace("@Nikey", "<@" + NikeyID + ">"))
-			
-		elif "@nikey" in message.content:	
-			await client.delete_message(message)
-			await client.send_message(message.channel, msg.replace("@nikey", "<@" + NikeyID + ">"))
+			smsg = re.sub("(?i)@nikey","<@" + NikeyID + ">", msg)
+			await client.send_message(message.channel, smsg)
 
 		#elif "@" + OtherPersonTag in message.content:
 		#	await client.delete_message(message)
